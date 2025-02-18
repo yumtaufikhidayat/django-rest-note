5e3b871a1491a866d23e64f78a68834ea66fca5b
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-18 09:02:56 +0700

    feat: add new model class

--- DjangoNote/notes/models.py
+++ DjangoNote/notes/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    tags = models.JSONField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
