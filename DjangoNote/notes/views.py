393e27bb8f0c019e6459cec480686fc3bc5c6ea3
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-18 09:03:04 +0700

    feat: add new view class.

--- DjangoNote/notes/views.py
+++ DjangoNote/notes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from notes.serializers import NoteSerializer
+
 
 # Create your views here.
+class NoteList(APIView):
+    def post(self, request):
+        note = NoteSerializer(data=request.data, context={'request': request})  # Pass request context
+        if note.is_valid(raise_exception=True):
+            note.save()
+            return Response(note.data, status=status.HTTP_201_CREATED)
+
+        return Response(note.errors, status=status.HTTP_400_BAD_REQUEST)
