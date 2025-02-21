a4ec2940f65141b096e6ebb3012a3096e4ce3cd8
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-21 10:36:09 +0700

    feat: add links to serializers.

--- DjangoNote/notes/serializers.py
+++ DjangoNote/notes/serializers.py
@@ -1,8 +1,28 @@
+from rest_framework.reverse import reverse
 from rest_framework import serializers
 from notes.models import Note
 
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
+    _links = serializers.SerializerMethodField()
+
     class Meta:
         model = Note
-        fields = ['id', 'title', 'body', 'tags', 'createdAt', 'updatedAt']
+        fields = ['id', 'title', 'body', 'tags', 'createdAt', 'updatedAt', '_links']
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return [
+            {
+                "rel": "self",
+                "href": reverse('note-list', request=request),
+                "action": "POST",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": reverse('note-detail', kwargs={'pk': obj.pk}, request=request),
+                "action": "GET",
+                "types": ["application/json"]
+            }
+        ]
