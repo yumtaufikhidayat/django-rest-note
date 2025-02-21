ec10834e37dabfbe64635693e7bae67c8e654aef
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-21 10:35:45 +0700

    feat: adjust url pattern for path notes and notes/{id} to handle note list and note detail.

--- DjangoNote/notes/urls.py
+++ DjangoNote/notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.NoteList.as_view()),
+    path('notes/', views.NoteList.as_view(), name='note-list'),
+    path('notes/<uuid:pk>/', views.NoteDetail.as_view(), name='note-detail')
 ]
