a72b92e7153fa77c8e23a81f2d215a4fc9400590
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-21 10:35:12 +0700

    feat: add note detail.

--- DjangoNote/notes/views.py
+++ DjangoNote/notes/views.py
@@ -1,3 +1,5 @@
+from django.core.serializers import serialize
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -21,3 +23,15 @@ class NoteList(APIView):
         return Response({
             "notes": serializer.data
         }, status=status.HTTP_200_OK)
+
+class NoteDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
