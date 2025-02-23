6a7b37d8a446e48a4331e4cc98c40814286253df
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-02-23 11:09:03 +0700

    feat: refactor NoteSerializer file description: - Renamed _links field to links and updated corresponding method from get__links to get_links. - Adjusted link rel values to accurately represent actions ("create" for POST on note-list, "self" for GET on note-detail, and "update" for PUT on note-detail).

--- DjangoNote/notes/urls.py
+++ DjangoNote/notes/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.NoteList.as_view(), name='note-list'),
-    path('notes/<uuid:pk>/', views.NoteDetail.as_view(), name='note-detail')
+    path('notes/<uuid:pk>', views.NoteDetail.as_view(), name='note-detail')
 ]
